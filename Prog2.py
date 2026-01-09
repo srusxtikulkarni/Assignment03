8b71aa208afc4ece777ad35777af4f407d1bcac2
srusxtikulkarni <shrushtikulkarni670@gmail.com>
2026-01-09 13:00:31 +0530

    Add files via upload

---
+++ Prog2.py
@@ -0,0 +1,22 @@
+age=int(input("enter your age:"))
+if age>=18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
+
+a=float(input("enter your emp_1 salary:"))
+b=float(input("enter your emp_2 salary:"))
+if a>b:
+    print("emp_1 earns more")
+else:
+    print("emp_2 earns more")
+
+
+pin=int(input("enter ur pin:"))
+if pin==82026:
+    print("verified pin is correct")
+else:
+    print("incorrect pin")
+    
+    
