b23499cf437792ab98ec939b2f0c931617a88851
srusxtikulkarni <shrushtikulkarni670@gmail.com>
2026-01-09 13:00:12 +0530

    Add files via upload

---
+++ Prog1.py
@@ -0,0 +1,16 @@
+bill=float(input("enter the bill amount:"))
+gst=bill*18/100
+total_amt=bill+gst
+print("Total Product Price:",total_amt)
+
+grp=int(input("enter total number of groups:"))
+student=int(input("enter total number of students:"))
+print(student%grp)
+
+
+voltage=int(input("enter voltage:"))
+res=int(input("enter res:"))
+p=voltage**2/res
+print(p)
+           
+           
